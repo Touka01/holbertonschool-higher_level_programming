daff53a78babe830e2ed797afd70c921e276b08f
Ahmed Toukebri <ahmedtoukebri0@gmail.com>
2023-05-29 18:03:01 +0100

    edges

---
+++ python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+first_3_word = word[:3]
+last_2_word = word[-2:]
+middle_word = word[1:-1]
+print(f"First 3 letters: {first_3_word}")
+print(f"Last 2 letters: {last_2_word}")
+print(f"Middle word: {middle_word}")
