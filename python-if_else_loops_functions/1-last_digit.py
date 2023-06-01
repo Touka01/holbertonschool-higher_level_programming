e0cb6ee6e77d3029b71a295f439b8c7fa3d3c7c2
Ahmed Toukebri <ahmedtoukebri0@gmail.com>
2023-06-01 11:58:03 +0000

    last digit

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -8,8 +8,13 @@ number = random.randint(-10000, 10000)
 # Get the last digit of the number
 last_digit = abs(number) % 10
 
+# Determine the sign of the last digit
+sign = ""
+if number < 0:
+    sign = "-"
+
 # Print the last digit with the corresponding message
-print("Last digit of", number, "is", last_digit, end=" ")
+print("Last digit of", sign + str(number), "is", sign + str(last_digit), end=" ")
 
 if last_digit > 5:
     print("and is greater than 5")
