6e792bfcb8840994ee1c23c2b556dbb256dc1796
Ahmed Toukebri <ahmedtoukebri0@gmail.com>
2023-06-12 18:28:28 +0000

    Score

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    x = 0
-    y = ""
-    if a_dictionary is None or a_dictionary == {}:
-        return(None)
-    for i in a_dictionary.keys():
-        if int(a_dictionary[i]) > x:
-            x = int(a_dictionary[i])
-            y = i
-    return(y)
+    if a_dictionary:
+        return(max(a_dictionary, key=a_dictionary.get))
