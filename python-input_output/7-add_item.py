d5edc442031c7654b9a134df86e635b8b86f570b
Ahmed Toukebri <ahmedtoukebri0@gmail.com>
2023-06-23 21:58:44 +0000

    addI

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -15,16 +15,20 @@ Contains function that adds and saves to Python obj to JSON file; loads objects
 
 """
 
-
+import os.path
+import subprocess
 from sys import argv
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 
-try:
+if os.path.isfile(filename):
     existing_content = load_from_json_file(filename)
-except FileNotFoundError:
+else:
     existing_content = []
 
 save_to_json_file(existing_content + argv[1:], filename)
+
+subprocess.run(['cat', filename])
+print("")
