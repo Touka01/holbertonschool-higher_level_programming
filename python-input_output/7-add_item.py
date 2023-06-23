7b37c1e38a6c6758ec0735b479ac262ae30fdd11
Ahmed Toukebri <ahmedtoukebri0@gmail.com>
2023-06-23 22:07:59 +0000

    kasarli rasi

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,21 +1,34 @@
 #!/usr/bin/python3
-"""Module for add_item method"""
+"""
+Module 7-add_item
 
-import json
-import sys
+Contains function that adds and saves to Python obj to JSON file; loads objects
+
+# run with ./7-add_item.py
+#
+# cat add_item.json ; echo ""
+# expect output: []
+#
+# ./7-add_item.py some random args
+# cat add_item.json ; echo ""
+# expect output: ["some", "random", "args"]
+
+"""
+
 import os.path
-
+import subprocess
+from sys import argv
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-"""script that adds all arguments to a Python list"""
 
-file = "add_item.json"
-json_list = []
+filename = "add_item.json"
 
-if os.path.exists(file):
-    json_list = load_from_json_file(file)
+if os.path.isfile(filename):
+    existing_content = load_from_json_file(filename)
+else:
+    existing_content = []
 
-for i in range(1, len(sys.argv)):
-    json_list.append(sys.argv[i])
+save_to_json_file(existing_content + argv[1:], filename)
 
-save_to_json_file(json_list, file)
+subprocess.run(['cat', filename])
+print("")
