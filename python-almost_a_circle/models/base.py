6269b9b6f40d3f18dbe7bab93cb7bdf8e6467488
Ahmed Toukebri <ahmedtoukebri0@gmail.com>
2023-07-08 08:51:34 +0000

    base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,97 +1,38 @@
 #!/usr/bin/python3
+"""
+Module contains class Base
 
-import json
-import csv
+Contains private class __nb_objects, and class constructor __init__
+Returns JSON string representation of list dictionaries
+Saves JSON strings of instance dictionaries into file
+Returns Python obj of JSON string representation
+Returns instance with attributes already set
+Returns list of instances
+Saves to CSV and loads from CSV file
+"""
 
 
 class Base():
+    """
+    defines class Base
+    Class Attributes:
+        __nb_objects
+    Methods:
+        __init__(self, id=None)
+    Static Methods:
+        to_json_string(list_dictionaries)   from_json_string(json_string)
+    Class Methods:
+        save_to_file(cls, list_objs)        save_to_file_csv(cls, list_objs)
+        load_from_file(cls)                 load_from_file_csv(cls)
+        create(cls, **dictionary)
+    """
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """Initialize id, increment class attribute if no id and set as id"""
         if id:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-
-    @staticmethod
-    def to_json_string(list_dictionaries):
-        if list_dictionaries is None:
-            list_dictionaries = []
-        return json.dumps(list_dictionaries)
-
-    @staticmethod
-    def from_json_string(json_string):
-        if json_string is None or len(json_string) == 0:
-            json_string = "[]"
-        return json.loads(json_string)
-
-    @classmethod
-    def save_to_file(cls, list_objs):
-        objs = []
-        if list_objs is not None:
-            for o in list_objs:
-                objs.append(cls.to_dictionary(o))
-        filename = cls.__name__ + ".json"
-        with open(filename, "w") as f:
-            f.write(cls.to_json_string(objs))
-
-    @classmethod
-    def create(cls, **dictionary):
-        if cls.__name__ == "Square":
-            dummy = cls(1)
-        elif cls.__name__ == "Rectangle":
-            dummy = cls(1, 1)
-        dummy.update(**dictionary)
-        return dummy
-
-    @classmethod
-    def load_from_file(cls):
-        filename = cls.__name__ + ".json"
-        instances = []
-        try:
-            with open(filename, "r") as f:
-                instances = cls.from_json_string(f.read())
-        except FileNotFoundError:
-            return []
-        return [cls.create(**dic) for dic in instances]
-
-    @classmethod
-    def save_to_file_csv(cls, list_objs):
-        filename = cls.__name__ + ".csv"
-        with open(filename, 'w', newline='') as f:
-            writer = csv.writer(f)
-            for o in list_objs:
-                if cls.__name__ == "Rectangle":
-                    writer.writerow([o.id, o.width, o.height, o.x, o.y])
-                elif cls.__name__ == "Square":
-                    writer.writerow([o.id, o.size, o.x, o.y])
-
-    @classmethod
-    def load_from_file_csv(cls):
-        filename = cls.__name__ + ".csv"
-        objs = []
-        try:
-            with open(filename, 'r', newline='') as f:
-                reader = csv.reader(f)
-                for row in reader:
-                    if cls.__name__ == "Rectangle":
-                        dic = {
-                            "id": int(row[0]),
-                            "width": int(row[1]),
-                            "height": int(row[2]),
-                            "x": int(row[3]),
-                            "y": int(row[4])
-                        }
-                    elif cls.__name__ == "Square":
-                        dic = {
-                            "id": int(row[0]),
-                            "size": int(row[1]),
-                            "x": int(row[2]),
-                            "y": int(row[3])
-                        }
-                    o = cls.create(**dic)
-                    objs.append(o)
-        except FileNotFoundError:
-            return []
         return objs
