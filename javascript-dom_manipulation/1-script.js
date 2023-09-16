37bedbfc57907b434818ea702ea23bc140a55ece
Ahmed Toukebri <ahmedtoukebri0@gmail.com>
2023-09-16 17:04:48 +0000

    update

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,6 @@
+const headerElement = document.querySelector('header'); // select the header element
+
+
+red_header.addEventListener('click', () => {
+    headerElement.style.color = '#FF0000';
+    }); // added event listener to the red_header id to change the color of the header element on click
